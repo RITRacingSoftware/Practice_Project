ee1d6c67cedc261abc6783813a0cf29de0693e6c
CRP5488 <crp5488@rit.edu>
2023-01-28 10:36:26 -0500

    added hal_aio

---
+++ src/driver/HAL_Aio/HAL_Aio.h
@@ -0,0 +1,25 @@
+#ifndef HAL_AIO
+#define HAL_AIO
+
+#include <stdint.h>
+
+typedef enum {
+    AIOpin_VOLTAGE,
+    AIOpin_ACCEL_A,
+    AIOpin_ACCEL_B,
+    AIOpin_NUM // Automatically is the value of the number of pins in the enum
+} AIOpin_e;
+
+/**
+ * Initialize ADC peripheral and other relevant peripherals for synchronous 12 bit reads.
+ */
+void HAL_Aio_init(void);
+
+/**
+ * Read the current voltage at the provided adc pin and report it using 12 bit granularity.
+ * pin - [in] provided adc pin
+ * return 0-4095
+ */
+uint16_t HAL_Aio_read(AIOpin_e pin);
+
+#endif // HAL_AIO
---
+++ src/driver/HAL_Aio/SIL_HAL_Aio.c
@@ -0,0 +1,12 @@
+// #include "HAL_Aio.h"
+// #include "VcCompat.h"
+
+// void HAL_Aio_init(void)
+// {
+//     // nothing required!
+// }
+
+// uint16_t HAL_Aio_read(AIOpin_e pin)
+// {
+//     return (uint16_t) ((hardware.aio[pin] / ADC_MAX_VOLTAGE) * ADC_MAX_VAL);
+// }
